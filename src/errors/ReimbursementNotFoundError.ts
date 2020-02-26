111f86c7bab1253097062b6d89fd3d4ac08fee7d
Anthony-Cona <acon5000@gmail.com>
2020-02-26 14:24:03 -0500

    Fixed minor spelling error in reimbursement Error

--- src/errors/ReimbursementNotFoundError.ts
+++ src/errors/ReimbursementNotFoundError.ts
@@ -2,6 +2,6 @@ import { HttpError } from "./HttpError";
 
 export class ReimbursementNotFoundError extends HttpError {
     constructor(){
-        super('Status Not Found', 404)
+        super('Reimbursement Not Found', 404)
     }
 }
